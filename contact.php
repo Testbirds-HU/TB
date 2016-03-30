0e242e6d2384ca9f2007b7bf61f72ee2ef3ddae2
Krishtof <danubiusinformatika@Daniels-MacBook-Pro.local>
2016-03-30 22:47:39 +0200

    contact php links

--- contact.php
+++ contact.php
@@ -89,11 +89,11 @@ $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
           <!-- Collect the nav links, forms, and other content for toggling-->
           <div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-              <li><a href="about-us.html" class="pageload-link">Rólunk</a></li>
-              <li><a href="benefits.html" class="pageload-link">Miért mi?</a></li>
-              <li><a href="functional.html" class="pageload-link">Funkcionális tesztelés</a></li>
-              <li><a href="user-experience.html" class="pageload-link">Felhasználói élmény kutatás</a></li>
-              <li><a href="contact.html" class="pageload-link">Kapcsolat</a></li>
+              <li><a href="about-us.html" class="pageload-link"><?=writeText($lang,'Menu1',$content);?></a></li>
+              <li><a href="benefits.html" class="pageload-link"><?=writeText($lang,'Menu2',$content);?></a></li>
+              <li><a href="functional.html" class="pageload-link"><?=writeText($lang,'Menu3',$content);?></a></li>
+              <li><a href="user-experience.html" class="pageload-link"><?=writeText($lang,'Menu4',$content);?></a></li>
+              <li><a href="contact.html" class="pageload-link"><?=writeText($lang,'Menu5',$content);?></a></li>
             </ul>
           </div>
         </div>
