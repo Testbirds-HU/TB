526a63d558185bbdc70fbe3b0ea4a02d8ea236e2
Krishtof <danubiusinformatika@Daniels-MacBook-Pro.local>
2016-03-31 11:37:58 +0200

    flickity link

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
     <link href="css/merge.css" rel="stylesheet">
     <link rel="stylesheet" href="css/okayNav.css" media="screen">
     <link rel="stylesheet" href="css/header.css" media="screen">
-    <link rel="stylesheet" href="css/flickity.css.css" media="screen">
+    <link rel="stylesheet" href="css/flickity.css" media="screen">
     <script src="js/flickity.pkgd.min.js"></script>
     <script src="bower_components/jquery/dist/jquery.min.js">   </script>
     <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
