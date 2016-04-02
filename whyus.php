0a66e284d3f3699413710813ff793b9efa084ad8
Krishtof <danubiusinformatika@Daniels-MacBook-Pro.local>
2016-04-02 22:41:08 +0200

    footer

--- whyus.php
+++ whyus.php
@@ -144,19 +144,14 @@
     </div>
       <footer class="container-fluid">
         <div class="row">
-          <div class="col-md-4">
-            <h5><?=writeText($lang,'footer_blog',$content);?></h5><a href="<?=writeText($lang,'blogurl2',$content);?>" class="pageload-link"><?=writeText($lang,'footer_blog2',$content);?><br></a><a href="<?=writeText($lang,'blogurl3',$content);?>" class="pageload-link"><?=writeText($lang,'footer_blog3',$content);?><br></a><a href="<?=writeText($lang,'blogurl4',$content);?>" class="pageload-link"><?=writeText($lang,'footer_blog4',$content);?><br></a><a href="<?=writeText($lang,'blogurl5',$content);?>" class="pageload-link"><?=writeText($lang,'footer_blog5',$content);?><br></a><a href="<?=writeText($lang,'blogurl6',$content);?>" class="pageload-link"><?=writeText($lang,'footer_blog6',$content);?><br></a>
-          </div>
-          <div class="col-md-2"></div>
-          <div class="col-md-3">
-            <h5><?=writeText($lang,'footer_oldalterkep',$content);?></h5><a href="rolunk.php" class="pageload-link"><?=writeText($lang,'footer_oldalterkep2',$content);?><br></a><a href="whyus.php" class="pageload-link"><?=writeText($lang,'footer_oldalterkep3',$content);?><br></a><a href="functional.php" class="pageload-link"><?=writeText($lang,'footer_oldalterkep4',$content);?><br></a><a href="ux.php" class="pageload-link"><?=writeText($lang,'footer_oldalterkep5',$content);?><br></a><a href="contact.php" class="pageload-link"><?=writeText($lang,'footer_oldalterkep6',$content);?><br></a>
-          </div>
-          <div class="col-md-3">
+          <div class="col-md-12 contact">
             <h5><?=writeText($lang,'footer_kapcsolat',$content);?></h5>
-            <p><?=writeText($lang,'footer_kapcsolat2',$content);?></p>
-            <p><?=writeText($lang,'footer_kapcsolat3',$content);?></p>
+            <p><?=writeText($lang,'footer_kapcsolat2',$content);?><br></p>
+            <p><?=writeText($lang,'footer_kapcsolat3',$content);?><br></p>
             <p><?=writeText($lang,'footer_kapcsolat4',$content);?></p>
-            <p><?=writeText($lang,'footer_kapcsolat5',$content);?></p>
+            <p><?=writeText($lang,'footer_kapcsolat5',$content);?></p><a href="facebok.com" class="ref">
+              <div class="fbicon"></div></a><a href="facebook.com" class="ref">
+              <div class="inicon"></div></a>
           </div>
         </div>
       </footer>
