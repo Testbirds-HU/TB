71d9a0b3772e0947ac393b2475bdaaa2fff07eea
Vízy <kristof.vizy@gmail.com>
2016-04-15 22:27:59 +0200

    references

--- index.php
+++ index.php
@@ -211,7 +211,7 @@
         </div>
         <div class="col-md-12 companies">
         <div class="row">
-
+          <div class="col-md-12"><h2><?=writeText($lang,'Referencia_cim',$content);?></h2></div>
           <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6"><img src="img/companies/1.png"></div>
           <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6"><img src="img/companies/2.png"></div>
           <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6"><img src="img/companies/3.png"></div>
