78b5cd54f38e7c1905f96f555e57771d35af6755
DecentM <DecentM@users.noreply.github.com>
2016-04-25 16:57:15 +0200

    Staging branch és takarítás

--- lang.php
+++ lang.php
@@ -1,5 +1,5 @@
 <?php
-	$content = json_decode(file_get_contents('lang2.json',true),true);	
+	$content = json_decode(file_get_contents('lang.json',true),true);	
 	function writeText($lang,$text,$json){
 		echo $json[$lang][$text];
 	}
