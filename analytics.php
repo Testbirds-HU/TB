83e8888efabf2b2767328fc1a702a7787d05d84d
DecentM <DecentM@users.noreply.github.com>
2016-04-28 15:04:58 +0200

    Analitika egységesítése

---
+++ analytics.php
@@ -0,0 +1,20 @@
+<?php
+	<script type="text/javascript">
+		var _paq = _paq || [];
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//analytics.testbirds.hu/";
+			_paq.push(['setTrackerUrl', u+'piwik.php']);
+			_paq.push(['setSiteId', 1]);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+	<noscript>
+		<p>
+			<img src="//analytics.testbirds.hu/piwik.php?idsite=1" style="border:0;" alt="" />
+		</p>
+	</noscript>
+	<script type="text/javascript" src="//js.leadin.com/js/v1/1798051.js" id="LeadinEmbed-1798051" crossorigin="use-credentials" async defer></script>
+?>
