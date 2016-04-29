506c844304657bd9059cc9b297c0b75b68d58c28
DecentM <DecentM@users.noreply.github.com>
2016-04-29 15:37:02 +0200

    Másolt sor javítása

--- analytics.php
+++ analytics.php
@@ -18,7 +18,7 @@
 <?php
 	if($lang == 'Czech'){
 		<script type="text/javascript" src="//js.leadin.com/js/v1/2223448.js" id="LeadinEmbed-2223448" crossorigin="use-credentials" async defer></script>
-	} elseif ($lang == 'Czech'){
+	} elseif ($lang == 'Slovak'){
 		# slovak tracking code
 	} else {
 		<script type="text/javascript" src="//js.leadin.com/js/v1/1798051.js" id="LeadinEmbed-1798051" crossorigin="use-credentials" async defer></script>
