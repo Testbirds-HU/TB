6993b5948f40c9a33327617966d9c6d4736954c9
DecentM <DecentM@users.noreply.github.com>
2016-05-02 15:41:14 +0200

    Fölös debug törlése

--- index.php
+++ index.php
@@ -37,7 +37,6 @@
     <script src="js/snap.svg-min.js"></script>
   </head>
   <body>
-    <span>URL is <?=$url?> lang is <?=$lang?>.</span>
     <div id="pagewrap">
       <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
