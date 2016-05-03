833186fe2e3a2f9b950b5bb682555493dab31269
DecentM <DecentM@users.noreply.github.com>
2016-05-03 13:25:22 +0200

    TLDExtract könyvtár használata

--- lang.php
+++ lang.php
@@ -1,13 +1,12 @@
 <?php
+require 'lib/tldx/tldextract.php';
+	
 	$content = json_decode(file_get_contents('lang.json',true),true);	
 	function writeText($lang,$text,$json){
 		echo $json[$lang][$text];
 	}
 
-  $tld = strrchr ( $_SERVER['SERVER_NAME'], "." );
-  $tld = substr ( $tld, 1 );
-
-  echo "TLD is $tld";
+  echo "TLD is $components->tld";
 
   if($tld == 'cz'){
     $lang = 'Czech';
