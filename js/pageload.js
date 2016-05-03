31ccfe915cb2053e6d4573fc701d8a0f69f22c1d
DecentM <DecentM@users.noreply.github.com>
2016-05-03 16:28:02 +0200

    Felületes reload nélküli oldalbetöltés

--- js/pageload.js
+++ js/pageload.js
@@ -1,24 +1,29 @@
-      (function() {
-          docInteractive('spill');
-          var pageWrap = document.getElementById('pagewrap'),
-              triggerLoading = [].slice.call(pageWrap.querySelectorAll('a.pageload-link')),
-              loader = new SVGLoader(document.getElementById('loader'), {
+
+      $(function() {
+          docInteractive(spill);
+          var pageWrap = document.getElementById(pagewrap),
+              triggerLoading = [].slice.call(pageWrap.querySelectorAll(a.pageload-link)),
+              loader = new SVGLoader(document.getElementById(loader), {
                   speedIn: 450
               });
 
           function init() {
               triggerLoading.forEach(function(trigger) {
-                  trigger.addEventListener('click', function(ev) {
+                  trigger.addEventListener(click, function(ev) {
                       ev.preventDefault();
-		      document.location.href = link;
                       loader.show();
                       setTimeout(function() {
                           loader.hide();
-                          linkTag = $(ev.target).is('a') ? $(ev.target) : $(ev.target).parents('a');
-                          link = $(linkTag).attr('href');
+                          linkTag = $(ev.target).is(a) ? $(ev.target) : $(ev.target).parents(a);
+                          link = $(linkTag).attr(href);
+                         //document.location.href = link;
+                         $("body").load(link);
+                         window.history.pushState(body, link, link);
                       }, 1000);
                   });
               });
           }
           init();
+
       })();
+
