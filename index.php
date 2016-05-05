769bb355ea8d5b477402043a7021564dccaf9290
DecentM <DecentM@users.noreply.github.com>
2016-05-05 16:41:55 +0200

    HTML head kivétele

--- index.php
+++ index.php
@@ -15,28 +15,7 @@
 </html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=writeText($lang,'head_title',$content);?></title>
-    <link href='https://fonts.googleapis.com/css?family=Droid+Sans:400,700|Roboto:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>    <meta name="description" content="<?=writeText($lang,'head_description',$content);?>">
-    <meta name="description" content="<?=writeText($lang,'head_description',$content);?>">
-    <meta name="keywords" content="<?=writeText($lang,'head_keywords',$content);?>">
-    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="shortcut icon" href="favicon.ico">
-    <link href="css/merge2.css" rel="stylesheet">
-    <link href="css/flickity.css" rel="stylesheet">
-    <link href="css/<?=writeText($lang,'customcss',$content);?>" rel="stylesheet">    
-    <link rel="stylesheet" href="css/header.css" media="screen">
-    <link href="css/<?=writeText($lang,'customcss',$content);?>" rel="stylesheet">
-    <link href="css/pageload.css" rel="stylesheet">
-    <script src="bower_components/jquery/dist/jquery.min.js">   </script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
-    <script src="js/scripts.js"></script>
-    <script src="js/flickity.pkgd.min.js"></script>    
-    <script src="js/snap.svg-min.js"></script>
-    <script src="js/pageload.js"></script>
+    <? include('html-head.php'); ?>
   </head>
   <body>
     <div id="pagewrap">
@@ -237,9 +216,6 @@
       </div>
     </div>
     <script src="js/classie.js"></script>
-    <script src="js/loader.js"></script>
-    <script src="js/svgLoader.js"></script>
-    <script src="js/pageload.js"></script>
 
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 
--- ux.php
+++ ux.php
@@ -10,24 +10,7 @@
 </html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=writeText($lang,'head_title',$content);?></title>
-    <link href='https://fonts.googleapis.com/css?family=Droid+Sans:400,700|Roboto:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>    <meta name="description" content="<?=writeText($lang,'head_description',$content);?>">
-    <meta name="description" content="<?=writeText($lang,'head_description',$content);?>">
-    <meta name="keywords" content="<?=writeText($lang,'head_keywords',$content);?>">
-    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="shortcut icon" href="favicon.ico">
-    <link href="css/merge2.css" rel="stylesheet">
-    <link href="css/<?=writeText($lang,'customcss',$content);?>" rel="stylesheet">    
-    <link href="css/<?=writeText($lang,'customcss',$content);?>" rel="stylesheet">    
-    <link rel="stylesheet" href="css/header.css" media="screen">
-    <script src="bower_components/jquery/dist/jquery.min.js">   </script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
-    <script src="js/scripts.js"></script>
-    <script src="js/snap.svg-min.js"></script>
+    <? include('html-head.php'); ?>
   </head>
   <body>
     <div id="pagewrap">
