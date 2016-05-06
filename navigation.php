bc88d8abf700670e4ace4e74b2e84cc1b382f36a
DecentM <DecentM@users.noreply.github.com>
2016-05-06 14:34:00 +0200

    Z-indexek visszaállítása a valóság talajára

--- navigation.php
+++ navigation.php
@@ -1,5 +1,5 @@
 <?php
-echo '<nav class="navbar navbar-default navbar-fixed-top" style="z-index: 1;">
+echo '<nav class="navbar navbar-default navbar-fixed-top" style="z-index: 4;">
         <div class="container-fluid">
           <div class="navbar-header">
             <button type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span
