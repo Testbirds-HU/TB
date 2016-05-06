77149f0f47016b79c88180d45c517322f3bacc96
DecentM <DecentM@users.noreply.github.com>
2016-05-06 14:00:49 +0200

    Pár css nem volt belinkelve

--- bottom-components.php
+++ bottom-components.php
@@ -1,14 +1,21 @@
 <?php echo '
     <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    
     <link href="css/merge2.css" rel="stylesheet">
     <link href="css/pageload.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/header.css" media="screen">
+    <link href="css/header.css" rel="stylesheet" media="screen">
+    <link href="css/colors.css" rel="stylesheet">
+    <link href="css/component.css" rel="stylesheet">
+    <link href="css/flickity.css" rel="stylesheet">
+    <link href="css/screen.css" rel="stylesheet">
+
     <script src="bower_components/jquery/dist/jquery.min.js">   </script>
     <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
     <script src="js/snap.svg-min.js" async defer></script>
     <script src="js/classie.js" async defer></script>
     <script src="js/pageload.js"></script>
+    
     <link href="css/basic-styles-'?><? echo($components->tld);?><? echo '.css" rel="stylesheet">
     <script>
     	$( document ).ready(function() {
