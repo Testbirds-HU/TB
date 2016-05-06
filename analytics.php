0770096a0a91a496f7a67c00574f47dad6ed5819
DecentM <DecentM@users.noreply.github.com>
2016-05-06 16:07:35 +0200

    Noscript analitika

--- analytics.php
+++ analytics.php
@@ -1,3 +1,4 @@
 <?php
-	echo("<script src=\"js/stats-");echo("$components->tld");echo(".js\" type=\"text/javascript\"></script>")
+	echo("<script src=\"js/stats-");echo("$components->tld");echo(".js\" type=\"text/javascript\"></script>");
+	echo('<noscript><p><img src="//analytics.testbirds.hu/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>');
 ?>
