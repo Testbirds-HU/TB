0287f488659981ced805ce1d216aef6cbf4488b7
DecentM <DecentM@users.noreply.github.com>
2016-05-06 14:21:55 +0200

    Footer egységesítése

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php echo('
+            <footer class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12 contact">
+                        <h5>')?><?=writeText($lang,'footer_kapcsolat',$content);?><? echo('</h5>
+                        <p>
+                            ')?><?=writeText($lang,'footer_kapcsolat2',$content);?><? echo('<br></p>
+                        <p>
+                            ')?><?=writeText($lang,'footer_kapcsolat3',$content);?><? echo('<br></p>
+                        <p>
+                            ')?><?=writeText($lang,'footer_kapcsolat4',$content);?><? echo('
+                        </p>
+                        <p>
+                            ')?><?=writeText($lang,'footer_kapcsolat5',$content);?><? echo('
+                        </p>
+                        <a href="https://www.facebook.com/testbirds.hu/" class="ref">
+                            <div class="fbicon"></div>
+                        </a>
+                        <a href="https://www.linkedin.com/company/testbirds-magyarorsz-g" class="ref">
+                            <div class="inicon"></div>
+                        </a>
+                    </div>
+                </div>
+            </footer>
+    ');
+?>
