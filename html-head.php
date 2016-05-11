b5da26ba3ae60aa7c5f234d28adc93873366cb1d
DecentM <DecentM@users.noreply.github.com>
2016-05-11 15:31:56 +0200

    Az egész Google cucc helyett csak a latin-extended kell

--- html-head.php
+++ html-head.php
@@ -2,7 +2,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href=\'https://fonts.googleapis.com/css?family=Droid+Sans:400,700|Roboto:400,700&subset=latin,latin-ext\' rel=\'stylesheet\' type=\'text/css\'>
     <meta name="description" content="'?><?=writeText($lang,'head_description',$content);?><? echo '">
     <meta name="description" content="'?><?=writeText($lang,'head_description',$content);?><? echo '">
     <meta name="keywords" content="'?><?=writeText($lang,'head_keywords',$content);?><? echo '">
