7580f5582932f09ef12f528a565f045e0614fe39
DecentM <DecentM@users.noreply.github.com>
2016-05-11 16:38:27 +0200

    CSS minimalizálása

--- bottom-components.php
+++ bottom-components.php
@@ -2,7 +2,7 @@
     <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
-    <link href="css/combined.css" rel="stylesheet">
+    <link href="css/combined.min.css" rel="stylesheet">
 
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
