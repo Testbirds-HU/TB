463b79deb2bdd630e236b18496cce82a02a2f27a
DecentM <DecentM@users.noreply.github.com>
2016-05-13 15:23:18 +0200

    Componensek ne csak a rootból töltődjenek be

--- bottom-components.php
+++ bottom-components.php
@@ -1,14 +1,14 @@
 <?php echo '
-    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
     <link href="css/combined.css" rel="stylesheet">
 
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="js/combined.js" async defer></script>
+    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="/js/combined.js" async defer></script>
 
-    <link href="css/basic-styles-'?><? echo($components->tld);?><? echo '.css" rel="stylesheet">
+    <link href="/css/basic-styles-'?><? echo($components->tld);?><? echo '.css" rel="stylesheet">
     <script>
     	$( document ).ready(function() {
    			$("#arrow").click(function() {
