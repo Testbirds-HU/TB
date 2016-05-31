1373356fa1c77ad6b23a0e7af88ad816770d94f6
DecentM <DecentM@users.noreply.github.com>
2016-05-31 16:05:18 +0200

    Képek optimalizálása css sprite-okkal

--- index.php
+++ index.php
@@ -180,30 +180,30 @@
                     <div class="row">
                         <div class="col-md-12" id="reftitle">
                             <h2><?=writeText($lang,'Referencia_cim',$content);?></h2></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6 sprite sprite-1"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6 sprite sprite-2"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6 sprite sprite-3"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6 sprite sprite-4"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6 sprite sprite-5"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6 sprite sprite-6"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6 sprite sprite-7"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6 sprite sprite-8"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 col-xs-6 sprite sprite-9"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 no sprite sprite-10"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 no sprite sprite-11"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 no sprite sprite-12"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 sprite sprite-13"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 sprite sprite-14"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 sprite sprite-15"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 sprite sprite-16"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 no sprite sprite-17"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 no sprite sprite-18"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 sprite sprite-19"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 sprite sprite-20"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 no sprite sprite-21"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 no sprite sprite-22"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 no sprite sprite-23"></div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 no sprite sprite-24"></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-1"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-2"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-3"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-4"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-5"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-6"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-7"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-8"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-9"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-10"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-11"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-12"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-13"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-14"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-15"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-16"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-17"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-18"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-19"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-20"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-21"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-22"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-23"></div></div>
+                        <div class="col-md-2 col-sm-4 col-xs-6"><div class="sprite sprite-24"></div></div>
                     </div>
                 </div>
             </div>
