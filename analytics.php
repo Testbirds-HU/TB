2f9c84b089ff7380570bb8926935200bab9004b2
DecentM <DecentM@users.noreply.github.com>
2016-05-31 16:42:30 +0200

    Annak nem ott kellene lennie, tripla analitika javítása

--- analytics.php
+++ analytics.php
@@ -1,4 +1,4 @@
 <?php
-	echo("<script src=\"js/source/stats-");echo("$components->tld");echo(".js\" type=\"text/javascript\" async defer></script>");
+	echo("<script src=\"js/stats-");echo("$components->tld");echo(".js\" type=\"text/javascript\" async defer></script>");
 	echo('<noscript><p><img src="//analytics.testbirds.hu/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>');
 ?>
