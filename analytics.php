01aa87652a795707d71716a0f4211b5df2bb19da
DecentM <DecentM@users.noreply.github.com>
2016-06-07 13:45:56 +0200

    Főoldal lead analitika

--- analytics.php
+++ analytics.php
@@ -1,4 +1,12 @@
 <?php
 	echo("<script src=\"js/stats-");echo("$components->tld");echo(".js\" type=\"text/javascript\" async defer></script>");
-	echo('<noscript><p><img src="//analytics.testbirds.hu/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>');
+  	echo('<noscript><p><img src="//analytics.testbirds.hu/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>');
+
+  if($tld == 'cz'){
+    echo('<!-- Start of Leadin Embed --><script type="text/javascript" src="//js.leadin.com/js/v1/2326791.js" id="LeadinEmbed-2326791" crossorigin="use-credentials" async defer></script><!-- End of Leadin Embed -->');
+  }elseif ($tld == 'sk'){
+   echo('<!-- Start of Leadin Embed --><script type="text/javascript" src="//js.leadin.com/js/v1/2326791.js" id="LeadinEmbed-2326791" crossorigin="use-credentials" async defer></script><!-- End of Leadin Embed -->');
+  }else{
+    echo('<!-- Start of Leadin Embed --><script type="text/javascript" src="//js.leadin.com/js/v1/1798051.js" id="LeadinEmbed-1798051" crossorigin="use-credentials" async defer></script><!-- End of Leadin Embed -->');
+  }	
 ?>
