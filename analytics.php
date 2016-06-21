50d4ef2286e41a452633786d7d3911b4cc58a789
DecentM <DecentM@users.noreply.github.com>
2016-06-21 14:45:28 +0200

    CZ Leadin

--- analytics.php
+++ analytics.php
@@ -3,7 +3,7 @@
   	echo('<noscript><p><img src="//analytics.testbirds.hu/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>');
 
   if($tld == 'cz'){
-    echo('<!-- Start of Leadin Embed --><script type="text/javascript" src="//js.leadin.com/js/v1/2326791.js" id="LeadinEmbed-2326791" crossorigin="use-credentials" async defer></script><!-- End of Leadin Embed -->');
+    echo('<!-- Start of Leadin Embed --><script type="text/javascript" src="//js.leadin.com/js/v1/2223448.js" id="LeadinEmbed-2223448" crossorigin="use-credentials" async defer></script><!-- End of Leadin Embed -->');
   }elseif ($tld == 'sk'){
    echo('<!-- Start of Leadin Embed --><script type="text/javascript" src="//js.leadin.com/js/v1/2326791.js" id="LeadinEmbed-2326791" crossorigin="use-credentials" async defer></script><!-- End of Leadin Embed -->');
   }else{
