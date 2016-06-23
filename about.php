d1a672d0dd37347f7c0dc64c80ebe6c12f558694
DecentM <DecentM@users.noreply.github.com>
2016-06-23 15:30:07 +0200

    Rólunk v1

--- about.php
+++ about.php
@@ -35,7 +35,7 @@
             </div>
             <div class="team container-fluid">
                 <div class="container">
-                    <h1>A csapat</h1>
+                    <h2>A csapat</h2>
 
 <!-- TEMPLATE:
 					<div class="row">
@@ -66,110 +66,11 @@
 -->
 
 					<div class="row">
-						<div class="col-md-3">
-							<div class="portrait anna"></div>
-						</div>
-						<div class="col-md-9">
-							<h2 class="text-centered">Mészár Anna</h2>
-							<span>
-								<p>
-									Annát leginkább az optimális folyamatok, a jó szolgáltatások, ügyfélbarát szemléletmód lelkesítik. Nagyra értékeli az akár apró, könnyen megvalósítható és mindeközben szellemes ötleteket, és azt a légkört, amelyben ezek az ötletek megszülethetnek. Kutatóként és kulturális antropológusként a probléma és a különböző nézőpontok árnyalt megértéséhez tud igazán hozzájárulni, ez az amiben leginkább otthon érzi magát.
-								</p>
-							</span>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-md-3 col-md-push-9">
-							<div class="portrait gizella"></div>
-						</div>
-						<div class="col-md-9 col-md-pull-3">
-							<h2 class="text-centered">Bartha Gizella</h2>
-							<span>
-								<p>
-									Gizella, a Testbirds UX kutatójaként közel 50 különböző termék mintegy 100 felhasználói kutatását végezte az elmúlt két évben. Hiszi, hogy egy jó termék létrehozásához nélkülözhetetlen a felhasználó bevonása a lehető legkorábban. Munkája során azon túl, hogy a kezdetektől alkalmazza az ismert módszertani megközelítéseket, folyamatosan törekszik arra, hogy ezek módosításával, megújításával még pontosabban tárja fel a felhasználók igényét.
-								</p>
-							</span>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-md-3">
-							<div class="portrait bianka"></div>
-						</div>
-						<div class="col-md-9">
-							<h2 class="text-centered">Makkai Bianka</h2>
-							<span>
-								<p>
-									Informatikai érdeklődése mellé közgazdász végzettség párosul, ezt leginkább funkcionális tesztelési projektekben kamatoztatja. Mindig az újdonságokat keresi, legyen szó fejlődésgazdaságtani elméletekről, gasztronómiáról vagy technológiai megoldásokról. Hisz benne, hogy bár az egész világot nem tehetjük jobbá, a munkánk révén egy szeletére mégis hatással tudunk lenni, ebből pedig a legjobbat kell kihoznunk.
-								</p>
-							</span>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-md-3 col-md-push-9">
-							<div class="portrait laszlo"></div>
-						</div>
-						<div class="col-md-9 col-md-pull-3">
-							<h2 class="text-centered">Horváth László</h2>
-							<span>
-								<p>
-									Szociológus végzettségű autodidakta fotós. Gyermekkora óta vonzódik a számítógépek és a programok világához. Érdekli minden olyan fejlesztés, kütyü, ami könnyebbé, érdekesebbé teszi a mindennapokat. Egyedi látásmódját, tudását és szenvedélyét összekapcsolva munkálkodik az ügyfelek és a felhasználók elégedettségén.
-								</p>
-							</span>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-md-3">
-							<div class="portrait agnes"></div>
-						</div>
-						<div class="col-md-9">
-							<h2 class="text-centered">Muszka Ágnes</h2>
-							<span>
-								<p>
-									Több éves tapasztalatot szerzett szoftvertesztelőként, valamint projektmenedzserként. Kommunikáció- és média szakon végzett a Budapesti Műszaki és Gazdaságtudományi Egyetemen. Érdeklődési területe a technológia és a közösségi együttműködések közötti összefüggések.
-								</p>
-							</span>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-md-3 col-md-push-9">
-							<div class="portrait gergely"></div>
-						</div>
-						<div class="col-md-9 col-md-pull-3">
-							<h2 class="text-centered">Vászon Gergely</h2>
-							<span>
-								<p>
-									Programtervező informatikus hallgató az ELTE-n. Tapasztalt sokszereplős, crowd illetve funkcionális teszt projektek menedzselésében, funkcionális és exploratív tesztelésben. 
-								</p>
-							</span>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-3">
-							<div class="portrait kata"></div>
-						</div>
-						<div class="col-md-9">
-							<h2 class="text-centered">Csankovszki Kata</h2>
-							<span>
-								<p>
-									Kata az új ügyfeleket kutatja fel, illetve a meglévő ügyfeleket segíti a közös munka további lehetőségeivel kapcsolatban a Testbirds-nél. Élete legmeghatározóbb célja, hogy segítse az embereket ott, ahol tudja. Így a Testbirds ügyfeleit például abban, hogy használhatóbb és szerethetőbb alkalmazásokat, weboldalakat és szoftvereket fejleszthessenek az ügyfeleiknek.
-									Kata balkezes, amivel egyáltalán nincs egyedül a Testbirds irodában. Talán ez a Testbirds sikereinek az egyik fontos indikátora
-
-								</p>
-							</span>
-						</div>
-					</div>
-
-					<div class="row">
 						<div class="col-md-3 col-md-push-9">
 							<div class="portrait tamas"></div>
 						</div>
 						<div class="col-md-9 col-md-pull-3">
-							<h2 class="text-centered">Lichter Tamás</h2>
+							<b class="text-centered">Lichter Tamás</b>
 							<span>
 								<p>
 									Telekommunikációs cégnél szerzett 10+ év tapasztalatot informatikai fejlesztési területen. Közgazdász végzettsége van, üzleti gondolkodásmód jellemzi (vagy legalábbis szeretné…). Tamás hisz abban, hogy egy cégnél akkor jó dolgozni, ha a vezetők megbíznak a munkatársakban, a munkatársakat bevonják a döntésekbe, nyitott, ügyfélorientált és haladó szemléletű a cégkultúra, és nem mellesleg jó a hangulat. Tamás azon dolgozik, hogy a Testbirds ilyen cég legyen. 
@@ -183,7 +84,7 @@
 							<div class="portrait akos"></div>
 						</div>
 						<div class="col-md-9">
-							<h2 class="text-centered">Maczinkó Ákos</h2>
+							<b class="text-centered">Maczinkó Ákos</b>
 							<span>
 								<p>
 									15+ év tapasztalat a telekommunikáció világában, elsősorban értékesítési és ügyfélélmény területen. Éveken keresztül részt vett lakossági termékek kialakításában és kereskedelmi elindításában. Ákos Tamással együtt vezeti a közép-európai Testbirds-öt, ami most már három országban működik.
@@ -194,10 +95,24 @@
 
 					<div class="row">
 						<div class="col-md-3 col-md-push-9">
-							<div class="portrait barnabas"></div>
+							<div class="portrait gizella"></div>
 						</div>
 						<div class="col-md-9 col-md-pull-3">
-							<h2 class="text-centered">Bertényi Barnabás</h2>
+							<b class="text-centered">Bartha Gizella</b>
+							<span>
+								<p>
+									Gizella, a Testbirds UX kutatójaként közel 50 különböző termék mintegy 100 felhasználói kutatását végezte az elmúlt két évben. Hiszi, hogy egy jó termék létrehozásához nélkülözhetetlen a felhasználó bevonása a lehető legkorábban. Munkája során azon túl, hogy a kezdetektől alkalmazza az ismert módszertani megközelítéseket, folyamatosan törekszik arra, hogy ezek módosításával, megújításával még pontosabban tárja fel a felhasználók igényét.
+								</p>
+							</span>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-3">
+							<div class="portrait barnabas"></div>
+						</div>
+						<div class="col-md-9">
+							<b class="text-centered">Bertényi Barnabás</b>
 							<span>
 								<p>
 									Barnabás pályáját crowdtesztelőként kezdte  és körülbelül aznap csatlakozott a Testbirds-höz, amint az legálisan lehetséges volt, alighogy a tizennyolcat betöltötte, az érettségi vizsgák után egyből az irodába jött dolgozni – vagyis ha valakire igaz, hogy lelkes fiatal, az Barnabás. 
@@ -205,6 +120,95 @@
 							</span>
 						</div>
 					</div>
+
+					<div class="row">
+						<div class="col-md-3 col-md-push-9">
+							<div class="portrait kata"></div>
+						</div>
+						<div class="col-md-9 col-md-pull-3">
+							<b class="text-centered">Csankovszki Kata</b>
+							<span>
+								<p>
+									Kata az új ügyfeleket kutatja fel, illetve a meglévő ügyfeleket segíti a közös munka további lehetőségeivel kapcsolatban a Testbirds-nél. Élete legmeghatározóbb célja, hogy segítse az embereket ott, ahol tudja. Így a Testbirds ügyfeleit például abban, hogy használhatóbb és szerethetőbb alkalmazásokat, weboldalakat és szoftvereket fejleszthessenek az ügyfeleiknek.
+									Kata balkezes, amivel egyáltalán nincs egyedül a Testbirds irodában. Talán ez a Testbirds sikereinek az egyik fontos indikátora
+
+								</p>
+							</span>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-3">
+							<div class="portrait laszlo"></div>
+						</div>
+						<div class="col-md-9">
+							<b class="text-centered">Horváth László</b>
+							<span>
+								<p>
+									Szociológus végzettségű autodidakta fotós. Gyermekkora óta vonzódik a számítógépek és a programok világához. Érdekli minden olyan fejlesztés, kütyü, ami könnyebbé, érdekesebbé teszi a mindennapokat. Egyedi látásmódját, tudását és szenvedélyét összekapcsolva munkálkodik az ügyfelek és a felhasználók elégedettségén.
+								</p>
+							</span>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-3 col-md-push-9">
+							<div class="portrait bianka"></div>
+						</div>
+						<div class="col-md-9 col-md-pull-3">
+							<b class="text-centered">Makkai Bianka</b>
+							<span>
+								<p>
+									Informatikai érdeklődése mellé közgazdász végzettség párosul, ezt leginkább funkcionális tesztelési projektekben kamatoztatja. Mindig az újdonságokat keresi, legyen szó fejlődésgazdaságtani elméletekről, gasztronómiáról vagy technológiai megoldásokról. Hisz benne, hogy bár az egész világot nem tehetjük jobbá, a munkánk révén egy szeletére mégis hatással tudunk lenni, ebből pedig a legjobbat kell kihoznunk.
+								</p>
+							</span>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-3">
+							<div class="portrait anna"></div>
+						</div>
+						<div class="col-md-9">
+							<b class="text-centered">Mészár Anna</b>
+							<span>
+								<p>
+									Annát leginkább az optimális folyamatok, a jó szolgáltatások, ügyfélbarát szemléletmód lelkesítik. Nagyra értékeli az akár apró, könnyen megvalósítható és mindeközben szellemes ötleteket, és azt a légkört, amelyben ezek az ötletek megszülethetnek. Kutatóként és kulturális antropológusként a probléma és a különböző nézőpontok árnyalt megértéséhez tud igazán hozzájárulni, ez az amiben leginkább otthon érzi magát.
+								</p>
+							</span>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-3 col-md-push-9">
+							<div class="portrait agnes"></div>
+						</div>
+						<div class="col-md-9 col-md-pull-3">
+							<b class="text-centered">Muszka Ágnes</b>
+							<span>
+								<p>
+									Több éves tapasztalatot szerzett szoftvertesztelőként, valamint projektmenedzserként. Kommunikáció- és média szakon végzett a Budapesti Műszaki és Gazdaságtudományi Egyetemen. Érdeklődési területe a technológia és a közösségi együttműködések közötti összefüggések.
+								</p>
+							</span>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-3">
+							<div class="portrait gergely"></div>
+						</div>
+						<div class="col-md-9">
+							<b class="text-centered">Vászon Gergely</b>
+							<span>
+								<p>
+									Programtervező informatikus hallgató az ELTE-n. Tapasztalt sokszereplős, crowd illetve funkcionális teszt projektek menedzselésében, funkcionális és exploratív tesztelésben. 
+								</p>
+							</span>
+						</div>
+					</div>
+
+
+
 
 
 
