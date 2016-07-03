fea0d4515fc09f2bfc359c3f09b9f6e3672ffda6
DecentM <DecentM@users.noreply.github.com>
2016-07-03 11:53:48 +0200

    Bower erőforrások egybekötése grunt feladat

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,21 @@ module.exports = function(grunt){
 	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
         // task configuration goes here
-
+        bower_concat: {
+        	main: {
+        		dest: {
+        			js: 'res/js/src/main.js',
+        			css: 'res/css/src/main.css'
+        		}
+        	}
+        }
     });
 
     // define the default task that executes when we run 'grunt' from inside the project
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', [
+    	'bower_concat'
+    ]);
 
 };
