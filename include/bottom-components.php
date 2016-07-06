22f9dc6b7fb804c8967d49c199fcde2cd688d3ee
DecentM <DecentM@users.noreply.github.com>
2016-07-06 13:26:37 +0200

    Kritikus CSS és javascriptek betöltésének optimalizálása

--- include/bottom-components.php
+++ include/bottom-components.php
@@ -1,13 +1,27 @@
 <?php echo '<link href="/res/css/basic-styles-'?><?php echo($components->tld);?><?php echo '.css" rel="stylesheet">';?>
-<script>
-    $(document).ready(function () {
-        $("#arrow").click(function () {
-            $("html, body").animate({
-                scrollTop: $("#top").offset().top - 90
-            }, 1000);
-        });
-    })
-</script>
-<script data-no-instant>
-    InstantClick.init();
-</script>
+            <script>
+                $(document).ready(function () {
+                    $("#arrow").click(function () {
+                        $("html, body").animate({
+                            scrollTop: $("#top").offset().top - 90
+                        }, 1000);
+                    });
+                })
+            </script>
+            <script type="text/javascript" src="/bower_components/jquery/dist/jquery.min.js"></script>
+            <script type="text/javascript">
+                $(document).ready(function () {
+                    console.log('Document ready fired');
+                    $.ajaxSetup({
+                        cache: true
+                    });
+                    $('head').append('<link rel="stylesheet" href="/res/css/dist/main.min.css" type="text/css" />');
+                    $.getScript("/res/js/dist/main.min.js", function () {
+                        console.log("Main js loaded");
+                        InstantClick.init();
+                    });
+                    $.getScript("/bower_components/bootstrap/dist/js/bootstrap.min.js", function () {
+                        console.log("Bootstrap js loaded");
+                    });
+                });
+            </script>
