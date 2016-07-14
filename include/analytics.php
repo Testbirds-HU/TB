fbf7715eafee67c73ea97e1e2f27946a35cb0dfd
DecentM <DecentM@users.noreply.github.com>
2016-07-14 14:31:09 +0200

    Ne legyen duplikált Leadin kód

--- include/analytics.php
+++ include/analytics.php
@@ -4,10 +4,10 @@
 <?php echo("<script src=\"/res/js/stats-");echo("$components->tld");echo(".js\" type=\"text/javascript\"></script>"); 
 
   if($tld == 'cz'){
-    echo('<!-- Start of Leadin Embed --><script type="text/javascript" src="//js.leadin.com/js/v1/2223448.js" id="LeadinEmbed-2223448" crossorigin="use-credentials"></script><!-- End of Leadin Embed -->');
+    echo('<!-- Start of Leadin Embed --><script data-no-instant type="text/javascript" src="//js.leadin.com/js/v1/2223448.js" id="LeadinEmbed-2223448" crossorigin="use-credentials"></script><!-- End of Leadin Embed -->');
   }elseif ($tld == 'sk'){
-   echo('<!-- Start of Leadin Embed --><script type="text/javascript" src="//js.leadin.com/js/v1/2326791.js" id="LeadinEmbed-2326791" crossorigin="use-credentials"></script><!-- End of Leadin Embed -->');
+   echo('<!-- Start of Leadin Embed --><script data-no-instant type="text/javascript" src="//js.leadin.com/js/v1/2326791.js" id="LeadinEmbed-2326791" crossorigin="use-credentials"></script><!-- End of Leadin Embed -->');
   }else{
-    echo('<!-- Start of Leadin Embed --><script type="text/javascript" src="//js.leadin.com/js/v1/1798051.js" id="LeadinEmbed-1798051" crossorigin="use-credentials"></script><!-- End of Leadin Embed -->');
+    echo('<!-- Start of Leadin Embed --><script data-no-instant type="text/javascript" src="//js.leadin.com/js/v1/1798051.js" id="LeadinEmbed-1798051" crossorigin="use-credentials"></script><!-- End of Leadin Embed -->');
   }	
 ?>
